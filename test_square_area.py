9c8119cfc10adc75c5e8f2c85b704efdfdd44fb1
SathuMahitha <157419038+SathuMahitha@users.noreply.github.com>
2024-03-16 20:54:10 -0400

    Update test_square_area.py

--- test_square_area.py
+++ test_square_area.py
@@ -5,7 +5,7 @@ from square import calculate_area
 
 def test_calculate_area():
     assert calculate_area(5) == 25  # Example unit test
-    assert calculate_area(49) == 6241  # Intentional failure, incorrect expected output
+    assert calculate_area(57) == 6241  # Intentional failure, incorrect expected output
 
 # Run the tests if this script is executed directly
 if __name__ == "__main__":
