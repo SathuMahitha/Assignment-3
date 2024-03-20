4b1e3c4afc79d99f5e58a406d5dabd25b2f37e44
SathuMahitha <157419038+SathuMahitha@users.noreply.github.com>
2024-03-20 16:59:23 -0400

    Update test_calculate_area_square_area.py

--- test_calculate_area_square_area.py
+++ test_calculate_area_square_area.py
@@ -3,6 +3,6 @@ import pytest
 from calculate_area_square import calculate_area_square
 
 def test_calculate_area_square():
-    length = 7  # You can use any arbitrary number here
-    expected_area = 49  # Changed the expected area to make the test fail
+    length = 57  # You can use any arbitrary number here
+    expected_area = 3249  # Changed the expected area to make the test fail
     assert calculate_area_square(length) == expected_are
